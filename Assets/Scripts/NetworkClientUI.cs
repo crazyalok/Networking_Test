2ff192732332bcfebfbd2f28b8c4476b28c6913e
crazyalok <alokmaurya159@gmail.com>
2020-04-20 09:10:18 +0530

    completed

--- Assets/Scripts/NetworkClientUI.cs
+++ Assets/Scripts/NetworkClientUI.cs
@@ -6,28 +6,19 @@ using UnityEngine.Networking.NetworkSystem;
 using UnityEngine.UI;
 public class NetworkClientUI : MonoBehaviour {
 	NetworkClient client;
-	public Text text, text1;
-	void OnGUI()
-	{
-		string ipaddress = Network.player.ipAddress;
-		GUI.Box(new Rect(10, Screen.height - 50, 100, 50), ipaddress);
-		GUI.Label(new Rect(20, Screen.height - 30, 100, 20), "Status : " + client.isConnected);
-		if (!client.isConnected)
-		{
-			if(GUI.Button(new Rect(10, 10, 60, 50), "Connect"))
-			{
-				connect();
-			}
-		}
-	}
+	public Text status;
+	public InputField ip;
+	public Button connection;
+	
 	void Start () {
 		client = new NetworkClient();
 		Input.gyro.enabled = true;
 		
 	}
-	void connect()
+	public void connect()
 	{
-		client.Connect("192.168.43.212", 25000);
+
+		client.Connect(ip.text, 25000);
 	}
 	
 	void Update () {
@@ -35,6 +26,8 @@ public class NetworkClientUI : MonoBehaviour {
 		var temp = temp1.eulerAngles;
 		if (client.isConnected)
 			{
+				connection.interactable = false;
+				status.text = "Connected";
 				StringMessage msg = new StringMessage();
 				msg.value = temp.x + "|" + temp.y + "|" + temp.z ;
 				client.Send(888, msg);
--- Assets/Scripts/NetworkServerUI.cs
+++ Assets/Scripts/NetworkServerUI.cs
@@ -3,17 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.Networking.NetworkSystem;
+using UnityEngine.UI;
 
 public class NetworkServerUI : MonoBehaviour {
-	private float speed = 20f;
-	void OnGUI()
-	{
-		string ipaddress = Network.player.ipAddress;
-		GUI.Box(new Rect(10,Screen.height - 50 , 100, 50), ipaddress);
-		GUI.Label(new Rect(20, Screen.height - 35, 100, 20), "Status : " + NetworkServer.active);
-		GUI.Label(new Rect(20, Screen.height - 20, 100, 20), "Connected : " + NetworkServer.connections.Count);
-	}
+	public Text ip;
 	void Start () {
+		string ipaddress = Network.player.ipAddress;
+		ip.text = "ip : " + ipaddress.ToString();
 		NetworkServer.Listen(25000);
 		NetworkServer.RegisterHandler(888, ServerRecieveMessage);
 	}
